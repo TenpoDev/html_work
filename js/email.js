4b9804506c46eb7a1d079bd0afc9fc2642709c37
BLANCO MARCO <marco.blanco@intesasanpaolo.com>
2024-11-19 22:36:40 +0100

    Email.js integrated

---
+++ js/email.js
@@ -0,0 +1,16 @@
+// Inizializza EmailJS
+emailjs.init("gY3ePzo5aizViUSwz"); // ID utente dalla dashboard di EmailJS
+
+// Gestisci l'invio del modulo
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Previeni il refresh della pagina
+
+    // Invia il modulo con EmailJS
+    emailjs.sendForm("service_fc1hdrp", "template_cziybgs", this)
+        .then(function() {
+            document.getElementById("contact-form").reset();
+            alert("Email inviata con successo!");
+        }, function(error) {
+            alert("Errore nell'invio: " + error.text);
+        });
+});
